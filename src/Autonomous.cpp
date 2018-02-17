435301c4558666bb5f22fb4dddb5b59bfc24fa01
Luc Lagarde <mlaga97@gmail.com>
2018-02-17 14:53:44 -0600

    Add switch-case statements for all autonomous possibilities

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -16,8 +16,102 @@ void Robot::AutonomousInit() {
 
 	// Get game data
 	std::string gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+
+	// Test that we got anything?
+	switch(autoMode) {
+
+		// Do nothing
+		case 1:
+			// TODO: Implement?
+			break;
+
+		// Go forward
+		case 2:
+			// TODO: Implement
+			break;
+
+		// Attempt switch
+		case 3:
+			switch(startingPosition) {
+				case 1:
+					if(gameData[0] == 'L') {
+						// Start on left, end on left switch
+						// TODO: Implement
+					} else {
+						// Start on left, end on right switch
+						// TODO: Implement
+					}
+					break;
+				case 2:
+					if(gameData[0] == 'L') {
+						// Start on center, end on left switch
+						// TODO: Implement
+					} else {
+						// Start on center, end on right switch
+						// TODO: Implement
+					}
+					break;
+				case 3:
+					if(gameData[0] == 'L') {
+						// Start on right, end on left switch
+						// TODO: Implement
+					} else {
+						// Start on right, end on right switch
+						// TODO: Implement
+					}
+					break;
+				default:
+					// Didn't select starting position!
+					// TODO: Decide what to do here. Perhaps just forward?
+					break;
+			}
+			break;
+
+		// Attempt scale
+		case 4:
+			switch(startingPosition) {
+				case 1:
+					if(gameData[0] == 'L') {
+						// Start on left, end on left scale
+						// TODO: Implement
+					} else {
+						// Start on left, end on right scale
+						// TODO: Implement
+					}
+					break;
+				case 2:
+					if(gameData[0] == 'L') {
+						// Start on center, end on left scale
+						// TODO: Implement
+					} else {
+						// Start on center, end on right scale
+						// TODO: Implement
+					}
+					break;
+				case 3:
+					if(gameData[0] == 'L') {
+						// Start on right, end on left scale
+						// TODO: Implement
+					} else {
+						// Start on right, end on right scale
+						// TODO: Implement
+					}
+					break;
+				default:
+					// Didn't select starting position!
+					// TODO: Decide what to do here. Perhaps just forward?
+					break;
+			}
+			break;
+
+		// Didn't select autonomous mode!
+		default:
+			// TODO: Decide what to do here. Perhaps just forward?
+			break;
+	}
 }
 
 void Robot::AutonomousPeriodic() {
 	Wait(0.005);
 }
+
